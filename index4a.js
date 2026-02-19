79ebce5c2e911edec9d75ae1d3c0faa0eccf2d17
Pathania1204 <garimapathania23@gmail.com>
2026-02-19 13:49:51 +0530

    done

--- index4a.js
+++ index4a.js
@@ -1,18 +1,18 @@
 import express from 'express'
 const app = express()
 // app.listen(8080, () => console.log("Server Started"))
-// app.get("/add/:a/:b", (req, res) => {
-//     const a = Number(req.params.a)
-//     const b = Number(req.params.b)
-//     const sum = a + b
+// app.get("/add/:x/:y", (req, res) => {
+//     const x = Number(req.params.x)
+//     const y = Number(req.params.y)
+//     const sum = x + y
 //     res.send(`Sum is ${sum}`)
 // })
 
 app.listen(8080, () => console.log("Server Started"))
-app.get("/add/a/:a/b/:b/c/:c", (req, res) => {
-    const a = Number(req.params.a)
-    const b = Number(req.params.b)
-    const c = Number(req.params.c)
-    const sum = a + b + c
+app.get("/add/x/:x/y/:y/z/:z", (req, res) => {
+    const x = Number(req.params.x)
+    const y = Number(req.params.y)
+    const z = Number(req.params.z)
+    const sum = x + y + z
     res.send(`Sum is ${sum}`)
 })

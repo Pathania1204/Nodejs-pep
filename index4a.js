24c1620a3bac988a7f080d794ac817d244130bfa
Pathania1204 <garimapathania23@gmail.com>
2026-02-19 13:56:34 +0530

    done

--- index4a.js
+++ index4a.js
@@ -8,11 +8,18 @@ const app = express()
 //     res.send(`Sum is ${sum}`)
 // })
 
+// app.listen(8080, () => console.log("Server Started"))
+// app.get("/add/x/:x/y/:y/z/:z", (req, res) => {
+//     const x = Number(req.params.x)
+//     const y = Number(req.params.y)
+//     const z = Number(req.params.z)
+//     const sum = x + y + z
+//     res.send(`Sum is ${sum}`)
+// })
+
 app.listen(8080, () => console.log("Server Started"))
-app.get("/add/x/:x/y/:y/z/:z", (req, res) => {
-    const x = Number(req.params.x)
-    const y = Number(req.params.y)
-    const z = Number(req.params.z)
-    const sum = x + y + z
-    res.send(`Sum is ${sum}`)
+app.get("/add/:a/:b/:c/:d", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    res.send("Hello World")
 })

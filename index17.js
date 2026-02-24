8f73aae810f619d11b4ff5efe23bed7fc4e9cd0b
Pathania1204 <garimapathania23@gmail.com>
2026-02-24 11:08:56 +0530

    all updated

---
+++ index17.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+app.listen(8080);
+const dbConnect = async () => {
+  await mongoose.connect("mongodb://localhost:27017/lpu26b");
+};
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  role: { type: String },
+});
+const userModel = await mongoose.model("users", userSchema);
+await dbConnect();
+
+app.get("/", async (req, res) => {
+  const result = await userModel.find(); // representing users collection
+  res.json(result);
+});
